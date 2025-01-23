b2376e53d088af9b4b32b58906ff3d987fc518fa
Alex <sascha.rumin@gmail.com>
2025-01-23 10:41:56 +0200

    Make edits in code structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-const NumOfBalls = 12;
-const OuterDiametr = 0.5; //m
-const wallWidth = 10e-3; //mm to m
-let innerDiametr = OuterDiametr - 2 * wallWidth;
-let innerRadius = 0;
+const NUM_OF_BALLS = 12;
+const MIN_OUTER_DIAMETR = 0.5; //m
+const WALL_WIDTH = 10e-3; //mm to m
+let innerDiametr = MIN_OUTER_DIAMETR - 2 * WALL_WIDTH;
+let innerRadius;
 let sumofInnerVolume = 0;
-for (let i = 1; i <= NumOfBalls; i++) {
+for (let i = 1; i <= NUM_OF_BALLS; i++) {
   innerRadius = innerDiametr / 2;
   let Volume = (4 / 3) * Math.PI * innerRadius ** 3;
   console.log("Volume " + i + ": " + Volume + " m^3");
   sumofInnerVolume += Volume;
-  innerDiametr += wallWidth * 2;
+  innerDiametr += WALL_WIDTH * 2;
 }
 console.log("Sum Of Volumes: " + sumofInnerVolume + " m^3");
